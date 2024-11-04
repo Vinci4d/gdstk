e403952cf3992ef384252b4828919ad988880da9
Nicholas Harrington <nick@vinci4d.ai>
2024-11-04 14:15:42 -0800

    Bumped conan version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class gdstkRecipe(ConanFile):
     name = "gdstk"
-    version = "0.9.55"
+    version = "0.9.56"
     package_type = "library"
 
     # Optional metadata
